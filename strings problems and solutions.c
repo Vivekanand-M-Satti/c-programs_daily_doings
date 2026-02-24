4f217eab8b53f59f27a4dd75544f52073a4cd8d9
Vivekanand-M-Satti <vivekanandmsatti@gmail.com>
2026-02-24 09:39:52 +0530

    Update strings problems and solutions.c

--- strings problems and solutions.c
+++ strings problems and solutions.c
@@ -1,1 +1,37 @@
+//----------------------------------------------------------------------24/02/2026------------------------
+//----------------------------1..............write the own string in the function/
+/*#include<stdio.h>
+void strin(char *str)
+{
+    printf("%s",str);
+}
+int main()
+{
+    char str1[]="hello mav";
+    strin (str1);
 
+}*/
+//-----------------------------2-------wap to convert the upper case to the lower ccase and the lower case to the upper case-
+#include<stdio.h>
+void change(char str[])
+{
+    for(int i=0;str[i]!=0;i++)
+    {
+        if(str[i]>='a'&&str[i]<='z')
+        {
+            printf("%c",str[i]=str[i]-32);
+        }
+        else if(str[i]>='A'&&str[i]<='Z')
+        {
+            printf("%c",str[i]=str[i]+32);
+            
+        }
+    }
+    printf("%s",str);
+
+}
+int main()
+{
+    char str[]="heLLlo";
+    change(str);
+}
